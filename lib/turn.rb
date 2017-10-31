d9f44b7163037c6121377c844cab6093e60f410e
Bperez4197 <bryceperez31@gmail.com>
2017-10-31 03:15:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,9 +28,8 @@ def valid_move?(board,index)
   index.between?(0,8) && !position_taken?(board,index)
 end
 
-def move(board, index, char)
-  board[0] = "O"
-  board[8] = "X"
+def move(board, index, char = "O")
+  board.between?[0,8] = "O" || "X"
 end
 
 def turn(board)
