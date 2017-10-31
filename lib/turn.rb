d48c3fb0ef5a2f0593792fe7f0f17632eb1670db
Bperez4197 <bryceperez31@gmail.com>
2017-10-31 03:28:44 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,7 @@ def move(board, index, char = "O")
   board[8] = "X"
 end
 
-def turn(board) loop do
+def turn(board) 
   puts "Please enter 1-9:"
   i = gets.strip
   index = input_to_index(i)
@@ -43,6 +43,5 @@ def turn(board) loop do
       puts "Sorry, that was an invalid move. Please enter 1-9:"
       turn(board)
   display_board(board)
-end
 end
 end
